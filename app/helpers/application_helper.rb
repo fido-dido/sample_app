98b2c8072837f69578bbbc4a358f9354a606c0c0
Luis Cano <luis.cano@gmail.com>
2016-12-30 23:13:18 +0000

    minor changes and added Guard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,4 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
-  
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   #get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
 
   #get 'about' => 'static_pages#about'
   #get 'contact' => 'static_pages#contact'
