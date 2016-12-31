983d1afb7a2c5fe71b87e18fe4f3d1d278149d56
Luis Cano <luis.cano@gmail.com>
2016-12-31 02:16:52 +0000

    added index, edit features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -18,7 +19,32 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @users = User.all
+  end
+  
   private 
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
   
   def user_params
     params.require(:user).permit(:name, :email,:password, 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,12 @@ class User < ActiveRecord::Base
   uniqueness: {case_sensitive: false}
   
   has_secure_password
-  validates:password, length: { minimum:6 }
+  validates:password, length: { minimum:6 }, allow_blank: true
+  
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
